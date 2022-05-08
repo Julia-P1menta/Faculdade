8dbc7ece84cca16f4c28776a3ed6571f462029ee
Julia-P1menta <86532010+Julia-P1menta@users.noreply.github.com>
2022-05-08 18:13:37 -0300

    Add files via upload

---
+++ Projeto_Inter.py
@@ -0,0 +1,30 @@
+num=(int(input("Digite um valor em Decimal: ")))
+
+#Conversor decimal para binario
+def decimalToBinary(num):
+    if (num > 0):
+        decimalToBinary(num // 2)
+    print(num % 2, end=' ')
+if __name__ == '__main__':
+    decimalToBinary(num)
+    print(":Binario")
+
+
+#Conversor decimal para Hexadecimal
+def decimalToBinary(num):
+    if (num > 0):
+        decimalToBinary(num // 16)
+    print(num % 16, end=' ')
+if __name__ == '__main__':
+    decimalToBinary(num)
+    print(":Hexadecimal")
+
+
+#Conversor decimal para Octadecimal
+def decimalToBinary(num):
+    if (num > 0):
+        decimalToBinary(num // 8)
+    print(num % 8, end=' ')
+if __name__ == '__main__':
+    decimalToBinary(num)
+    print(":Ocatadecimal")
