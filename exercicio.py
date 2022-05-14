75bba0d5ae2812eab08aa050143a77132116986c
Julia-P1menta <86532010+Julia-P1menta@users.noreply.github.com>
2022-05-14 15:40:43 -0300

    Update and rename exercicio_06.py to exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -1,3 +1,11 @@
+#Uma academia deseja fazer um senso entre seus clientes para descobrir o mais alto, o 
+#mais baixo, a mais gordo e o mais magro, para isto você deve fazer um programa que 
+#pergunte a cada um dos clientes da academia seu código, sua altura e seu peso. O final 
+#da digitação de dados deve ocorrer quando o usuário digitar 0 (zero) no campo código. 
+#Ao encerrar o programa deve ser informados os códigos e os valores do cliente mais 
+#alto, do mais baixo, do mais gordo e do mais magro, além da média das alturas e dos 
+#pesos dos clientes.
+
 cod_gordo = 0
 cod_magro = 0
 cod_alto = 0
